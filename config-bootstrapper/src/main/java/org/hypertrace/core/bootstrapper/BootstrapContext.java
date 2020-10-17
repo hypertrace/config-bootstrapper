8ed2398b26329d69a2348ee6d97baa52359512bf
Buchi Reddy Busi Reddy <buchi@traceable.ai>
2020-10-16 17:02:13 -0700

    Config bootstrapping support for new entity type service.

--- config-bootstrapper/src/main/java/org/hypertrace/core/bootstrapper/BootstrapContext.java
+++ config-bootstrapper/src/main/java/org/hypertrace/core/bootstrapper/BootstrapContext.java
@@ -3,9 +3,7 @@ package org.hypertrace.core.bootstrapper;
 import com.typesafe.config.Config;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 import org.hypertrace.core.attribute.service.client.AttributeServiceClient;
 import org.hypertrace.core.attribute.service.client.config.AttributeServiceClientConfig;
 import org.hypertrace.entity.service.client.config.EntityServiceClientConfig;
@@ -18,26 +16,32 @@ public class BootstrapContext {
 
   private final AttributeServiceClient attributesServiceClient;
   private final EntityTypeServiceClient entityTypeServiceClient;
+  private final org.hypertrace.entity.type.client.EntityTypeServiceClient entityTypeServiceClientV2;
 
-  private final ManagedChannel attributeServiceChannel;
-  private final ManagedChannel entityServiceChannel;
   private final List<ManagedChannel> managedChannels;
 
   private BootstrapContext(
       AttributeServiceClientConfig attributeServiceClientConfig,
       EntityServiceClientConfig entityServiceClientConfig) {
 
-    this.attributeServiceChannel = ManagedChannelBuilder.forAddress(
+    ManagedChannel attributeServiceChannel = ManagedChannelBuilder.forAddress(
         attributeServiceClientConfig.getHost(), attributeServiceClientConfig.getPort())
         .usePlaintext().build();
     this.attributesServiceClient = new AttributeServiceClient(attributeServiceChannel);
 
-    this.entityServiceChannel = ManagedChannelBuilder.forAddress(
+    ManagedChannel entityServiceChannel = ManagedChannelBuilder.forAddress(
         entityServiceClientConfig.getHost(), entityServiceClientConfig.getPort())
         .usePlaintext().build();
     this.entityTypeServiceClient = new EntityTypeServiceClient(entityServiceChannel);
 
-    managedChannels = List.of(attributeServiceChannel, entityServiceChannel);
+    ManagedChannel entityTypeServiceChannel = ManagedChannelBuilder.forAddress(
+        entityServiceClientConfig.getHost(), entityServiceClientConfig.getPort())
+        .usePlaintext().build();
+    this.entityTypeServiceClientV2 =
+        new org.hypertrace.entity.type.client.EntityTypeServiceClient(entityTypeServiceChannel);
+
+    managedChannels = List.of(attributeServiceChannel, entityServiceChannel,
+        entityTypeServiceChannel);
   }
 
   public static BootstrapContext buildFrom(Config config) {
@@ -51,6 +55,10 @@ public class BootstrapContext {
 
   public EntityTypeServiceClient getEntityTypeServiceClient() {
     return entityTypeServiceClient;
+  }
+
+  public org.hypertrace.entity.type.client.EntityTypeServiceClient getEntityTypeServiceClientV2() {
+    return entityTypeServiceClientV2;
   }
 
   public void close() {
