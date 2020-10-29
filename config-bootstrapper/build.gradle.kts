00ddb257472c48962ba51e748d7daf4b31f79c45
Ravi Singal <ravi.singal@traceable.ai>
2020-10-29 17:45:37 +0530

    fix snyk-scan failure

--- config-bootstrapper/build.gradle.kts
+++ config-bootstrapper/build.gradle.kts
@@ -155,6 +155,12 @@ dependencies {
     because("https://snyk.io/vuln/SNYK-JAVA-IONETTY-1020439")
   }
 
+  constraints {
+    api("com.google.guava:guava:30.0-android") {
+      because("Information Disclosure [Medium Severity][https://snyk.io/vuln/SNYK-JAVA-COMGOOGLEGUAVA-1015415] in com.google.guava:guava@29.0-android")
+    }
+  }
+
   testImplementation("org.junit.jupiter:junit-jupiter:5.6.2")
   testImplementation("org.mockito:mockito-core:3.3.3")
 
